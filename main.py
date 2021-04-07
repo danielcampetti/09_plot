0e8e0adedb2e93a3ae9d864741097310229ebe11
Daniel Campetti <danielcampetti@gmail.com>
2021-04-07 13:33:19 -0300

    001

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from PIL import ImageTk, Image
-from tkinter import *
-# v005
+#Programa da interação entre Python e PowerBi
 
-root = Tk()
-root.title("Aplicativo Nieto Engenharia")
-root.iconbitmap('.//img//logo.ico')
-root.geometry("400x400")
+#region import
+import matplotlib.pyplot as plt
+import math
+#endregion
 
+#region dados iniciais
+L = 10  # largura total do galpao
+Ag = 5  # altura do pé direito do galpao
+At = 1  # altura do pé direito do telhado
+Nn = 3  # Numeros de V
+dT = 2  # 'Meio Comprimento do banzo de contato topo tesouras direitos e esquerdos
+h = 0.4  # Largura da tesoura
+a = 0.3  # Comnprimento a
+Cg = 15  # Comprimento do galpao
+Npo = 3  # Numero de porticos
+T1 = 0.05
+T2 = 0.04
+#endregion
 
-button_quit = Button(root, text="Fechar Programa", command=root.quit)
-button_quit.pack()
-
-root.mainloop()
+#region soluções iniciais
+Np = Nn*2
+d = dT/2
+Kt = (L/2)-a-d
+Alpha = math.atan(At/Kt)
+b = math.asin(Alpha)*h
+c = math.tan(Alpha)*b
+e = h-(a*At/Kt)
+f = math.sin(Alpha)*h
+j = math.tan(Alpha)*d
+# valor do passo de comprimento em 'y' de cada meio V
+Ap = (At-c)/Np
+K = Kt-b
+Kp = K/Np  # valor do passo de comprimento em 'x' de cada meio V
+#endregion
