5acc2e72be70b9fadf2e845dfffba4d9f57794cf
Daniel Campetti <danielcampetti@gmail.com>
2021-04-07 10:44:18 -0300

    006

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-v005
+from PIL import ImageTk, Image
+from tkinter import *
+# v005
 
-#hdhjahpouhfoujasdoufasohfiojsadhf
-
-
-
-
-
-
-
+root = Tk()
+root.title("Aplicativo Nieto Engenharia")
+root.iconbitmap('.//img//logo.ico')
+root.geometry("400x400")
 
 
+button_quit = Button(root, text="Fechar Programa", command=root.quit)
+button_quit.pack()
 
-
+root.mainloop()
