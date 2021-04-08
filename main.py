23b44fb48ad0c5183056fc48f9d755998ca98939
Daniel Campetti <danielcampetti@gmail.com>
2021-04-08 11:21:57 -0300

    014

--- main.py
+++ main.py
@@ -62,37 +62,34 @@ plt.axis([0, 6, 0, 20])
 root = tk.Tk()
 root.title("Aplicativo Nieto Engenharia")
 root.iconbitmap('.//img//logo.ico')
-# root.minsize(1000, 700)
-# root.rowconfigure(0, weight=1)
-# root.columnconfigure(0, weight=1)
+root.minsize(1500, 900)
 
-# frame1 = tk.Frame(root, bg='#abd1db')
-# # frame1.place(relx=0, rely=0, relwidth=1, relheight=1)
-# frame1.grid(row=0, column=0, sticky='N')
-
-# height=1000, width=1000
 frame1 = tk.LabelFrame(root, text="LabelFrame 1", bg='#6473c4')
 frame1.grid(row=0, column=0, sticky='N')
-
 frame2 = tk.LabelFrame(root, text="LabelFrame 2", bg='#3c3e4d')
 frame2.grid(row=1, column=0, sticky='N')
 
 x2 = np.array(range(50))
 y2 = x2*3
-df = pd.DataFrame(
-    {'A': x2, 'B': y2}, index=range(50))
-
-
+df = pd.DataFrame({'A': x2, 'B': y2}, index=range(50))
 # fig = Figure()
 figure1 = plt.Figure(figsize=(5, 4), dpi=80)
-ax1 = figure1.add_subplot(111)
+ax1 = figure1.add_subplot(111).plot(x2,y2)
 canvas = FigureCanvasTkAgg(figure1, frame1)
-# canvas.draw()
+canvas.draw()
 canvas.get_tk_widget().grid(row=1, column=0, sticky='N')
-
-df = df.iloc[:, 1]
-df.plot(kind='line', legend=True, ax=ax1, color='r', marker='o', fontsize=10)
 ax1.set_title('Initial data')
+
+# toolbar = NavigationToolbar2Tk(canvas, root)
+# toolbar.update()
+# canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+# def on_key(event):
+#     print('press')
+#     key_press_handler(event, canvas, toolbar)
+# canvas.mpl_connect('key', on_key)
+
+
+
 
 button_quit = tk.Button(frame1, text="10", command=root.quit, padx=50, pady=50)
 button_quit.grid(row=0, column=0, sticky='N')
@@ -100,20 +97,11 @@ button_quit.grid(row=0, column=0, sticky='N')
 button_quit1 = tk.Button(frame2, text="11", command=root.quit)
 button_quit1.grid(row=0, column=0, sticky='N')
 
-# button_quit2 = tk.Button(frame2, text="12", command=root.quit)
-# button_quit2.grid(row=3, column=0, sticky='N')
-
 print('-------------------Complete--------------------------------')
 root.mainloop()
 
 
-# toolbar = NavigationToolbar2Tk(canvas, root)
-# toolbar.update()
-# def on_key(event):
-#     print('press')
-#     key_press_handler(event, canvas, toolbar)
 
-# canvas.mpl_connect('key', on_key)
 
 # frame1 = tk.Frame(root, bg='#abd1db')
 # frame1.place(relx=0, rely=0, relwidth=1, relheight=1)
